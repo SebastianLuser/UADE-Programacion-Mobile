15135c349764b2e4c211835fbfc58c8e830eb05e
Julian Fausto <julianfausto09@gmail.com>
2025-11-25 01:25:54 -0300

    Vault UI

--- Assets/2. Scripts/Gameplay/VaultController.cs
+++ Assets/2. Scripts/Gameplay/VaultController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Services;
 using Services.MicroServices.EventsServices;
@@ -10,7 +11,8 @@ namespace _2._Scripts.Gameplay
     {
         [SerializeField] private Transform vaultDoorTransform;
         [SerializeField] private float timeToForceOpen = 2f;
-        [SerializeField] private AnimationCurve  openAnimationCurve;
+        [SerializeField] private float openAnimationTime = 2f;
+        [SerializeField] private float angleToOpen = 120f;
         
         private Coroutine m_animationCoroutine;
         private float m_forceOpenTimer;
@@ -18,9 +20,14 @@ namespace _2._Scripts.Gameplay
         
         private static IEventService EventService => ServiceLocator.Get<IEventService>();
 
+        private void Awake()
+        {
+            m_forceOpenTimer = timeToForceOpen;
+        }
+
         private void OpenDoor()
         {
-            if (m_animationCoroutine != null) 
+            if (m_animationCoroutine != null || m_isDoorOpen) 
                 return;
             
             EventService.DispatchEvent(EventsDefinition.OPEN_VAULT);
@@ -31,22 +38,27 @@ namespace _2._Scripts.Gameplay
         private IEnumerator OpenDoorCoroutine()
         {
             var l_timer = 0f;
-            var l_maxTime = openAnimationCurve.keys[openAnimationCurve.length - 1].time;
-
-            while (l_timer < l_maxTime) 
+            var l_rotation = vaultDoorTransform.rotation;
+            var l_rotationFinal = Quaternion.Euler(0f, -angleToOpen, 0f);
+            
+            while (l_timer < openAnimationTime) 
             {
-                vaultDoorTransform.Rotate(0f, openAnimationCurve.Evaluate(l_timer), 0f);
                 l_timer += Time.deltaTime;
+                vaultDoorTransform.rotation = Quaternion.Slerp(l_rotation, l_rotationFinal, l_timer / openAnimationTime);
                 yield return null;
             }
+            
             EventService.DispatchEvent(EventsDefinition.END_OPEN_VAULT);
             m_animationCoroutine = null;
         }
 
         private void OnTriggerEnter(Collider p_other)
         {
-            if (p_other.CompareTag("Player"))
-                EventService.DispatchEvent(EventsDefinition.START_OPEN_VAULT);
+            if (!p_other.CompareTag("Player")) 
+                return;
+            
+            m_forceOpenTimer = timeToForceOpen;
+            EventService.DispatchEvent(EventsDefinition.START_OPEN_VAULT);
         }
 
         private void OnTriggerStay(Collider p_other)
@@ -57,8 +69,9 @@ namespace _2._Scripts.Gameplay
             if (!p_other.CompareTag("Player")) 
                 return;
             
-            m_forceOpenTimer += Time.deltaTime;
-            if (m_forceOpenTimer >= timeToForceOpen)
+            m_forceOpenTimer -= Time.deltaTime;
+            EventService.DispatchEvent(new OnUpdateVaultEvent(m_forceOpenTimer));
+            if (m_forceOpenTimer <= 0f)
             {
                 OpenDoor();
             }
@@ -72,8 +85,18 @@ namespace _2._Scripts.Gameplay
             if (!p_other.CompareTag("Player")) 
                 return;
             
-            m_forceOpenTimer = 0f;
+            m_forceOpenTimer = timeToForceOpen;
             EventService.DispatchEvent(EventsDefinition.END_OPEN_VAULT);
+        }
+    }
+
+    public struct OnUpdateVaultEvent : ICustomEventData
+    {
+        public readonly float Seconds;
+
+        public OnUpdateVaultEvent(float p_seconds)
+        {
+            Seconds = p_seconds;
         }
     }
 }
